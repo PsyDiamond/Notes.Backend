23b3602a14346872713a34a622834fa26393c138
Lex Talionis <lextalionis1488@gmail.com>
2021-08-20 06:18:16 +0100

    Ядро для автомапера

---
+++ Notes.Application/Common/Mappings/AssemblyMappingProfile.cs
@@ -0,0 +1,43 @@
+﻿using AutoMapper;
+using System;
+using System.Linq;
+using System.Reflection;
+
+namespace Notes.Application.Common.Mappings
+{
+    /// <summary>
+    /// Создание автомаперов из сборки
+    /// </summary>
+    public class AssemblyMappingProfile : Profile
+    {
+        /// <summary>
+        /// Создание автомаперов
+        /// </summary>
+        /// <param name="assembly">сборка</param>
+        public AssemblyMappingProfile(Assembly assembly) => ApplyMappingsFromAssebmly(assembly);
+
+        /// <summary>
+        /// Ядро автомапера
+        /// </summary>
+        /// <param name="assembly">сборка</param>
+        private void ApplyMappingsFromAssebmly(Assembly assembly)
+        {
+            // Получение типов, которые реализуют IMapWith<>
+            var types = assembly.GetExportedTypes()
+                .Where(type => type.GetInterfaces()
+                    .Any(x => x.IsGenericType 
+                        && 
+                        x.GetGenericTypeDefinition() == typeof(IMapWith<>)
+                        )
+                    ).ToList();
+
+            // Создание всех этих типов и вызов у них метода Mapping
+            foreach (var type in types)
+            {
+                var instance = Activator.CreateInstance(type);
+                var methodInfo = type.GetMethod("Mapping");
+                methodInfo?.Invoke(instance, new[] { this });
+            }
+        }
+    }
+}
---
+++ Notes.Application/Common/Mappings/IMapWith.cs
@@ -0,0 +1,17 @@
+﻿using AutoMapper;
+
+namespace Notes.Application.Common.Mappings
+{
+    /// <summary>
+    /// Интефейс маппинга
+    /// </summary>
+    /// <typeparam name="T">какую сущность надо смапить на другую</typeparam>
+    public interface IMapWith<T>
+    {
+        /// <summary>
+        /// Отобразить
+        /// </summary>
+        /// <param name="profile">профиль маппинга</param>
+        void Mapping(Profile profile) => profile.CreateMap(typeof(T), GetType());
+    }
+}
