0f8d64ba0613fa8682d08dbe8c87c5577f2ca3a2
Lex Talionis <lextalionis1488@gmail.com>
2021-08-20 04:45:38 +0100

    Добавьте файлы проекта.

---
+++ Notes.Domain/Note.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace Notes.Domain
+{
+    /// <summary>
+    /// Заметка
+    /// </summary>
+    public class Note
+    {
+        /// <summary>
+        /// Идентификатор пользователя
+        /// </summary>
+        public Guid UserId { get; set; }
+
+        /// <summary>
+        /// Идентификатор заметки
+        /// </summary>
+        public Guid Id { get; set; }
+
+        /// <summary>
+        /// Заголовок (название)
+        /// </summary>
+        public string Title { get; set; }
+
+        /// <summary>
+        /// Детали (содержимое заметки)
+        /// </summary>
+        public string Details { get; set; }
+
+        /// <summary>
+        /// Дата созадания заметки
+        /// </summary>
+        public DateTime CreationDate { get; set; }
+
+        /// <summary>
+        /// Дата изменения заметки
+        /// </summary>
+        public DateTime? EditDate { get; set; }
+    }
+}
