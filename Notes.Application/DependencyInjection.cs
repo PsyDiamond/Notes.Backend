294bf7ad7e2dbaea04473ad477844bdb6ef18e1f
Lex Talionis <lextalionis1488@gmail.com>
2021-08-21 03:06:00 +0100

    Методы расширения для DI

---
+++ Notes.Application/DependencyInjection.cs
@@ -0,0 +1,26 @@
+﻿using MediatR;
+using Microsoft.Extensions.DependencyInjection;
+using System.Reflection;
+
+namespace Notes.Application
+{
+    /// <summary>
+    /// Класс для методов раширения
+    /// </summary>
+    public static class DependencyInjection
+    {
+        /// <summary>
+        /// Добавить поддержку паттерна Медиатор
+        /// </summary>
+        /// <param name="services">коллекция сервисов</param>
+        /// <returns>коллекция сервисов</returns>
+        public static IServiceCollection AddApplication(this IServiceCollection services)
+        {
+            // Активировать все медиаторы из текущей сборки
+            services.AddMediatR(Assembly.GetExecutingAssembly());
+
+            // Передать управление дальше
+            return services;
+        }
+    }
+}
---
+++ Notes.Persistence/DependencyInjection.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Notes.Application.Interfaces;
+
+namespace Notes.Persistence
+{
+    /// <summary>
+    /// Класс для метода расширения
+    /// </summary>
+    public static class DependencyInjection
+    {
+        /// <summary>
+        /// Добавить функции хранения в DI
+        /// </summary>
+        /// <param name="services">коллекция сервисов</param>
+        /// <param name="configuration">конфигурация</param>
+        /// <returns>коллекция сервисов</returns>
+        public static IServiceCollection AddPersistence(this IServiceCollection services, IConfiguration configuration)
+        {
+            // Строка подключения из файла
+            var connectionString = configuration["DbConnection"];
+            // Связать dbcontext с реальной базой
+            services.AddDbContext<NotesDbContext>(options => options.UseSqlite(connectionString) );
+            // Публикую dbcontext
+            services.AddScoped<INotesDbContext>(provider => provider.GetService<NotesDbContext>());
+
+            // Передаю управление дальше
+            return services;
+        }
+    }
+}
