8130d83912cbe6ddbf5beedca62085f6ec99c64b
Lex Talionis <lextalionis1488@gmail.com>
2022-05-20 09:10:16 +0100

    Add Geters notes tests

---
+++ Notes.Tests/Notes/Queries/GetNoteDetailsQueryHandlerTests.cs
@@ -0,0 +1,45 @@
+﻿using AutoMapper;
+using Notes.Application.Notes.Queries.GetNoteDetails;
+using Notes.Persistence;
+using Notes.Tests.Common;
+using Shouldly;
+using System;
+using System.Threading;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace Notes.Tests.Notes.Queries
+{
+    [Collection("QueryCollection")]
+    public class GetNoteDetailsQueryHandlerTests
+    {
+        readonly NotesDbContext Context;
+        readonly IMapper Mapper;
+
+        public GetNoteDetailsQueryHandlerTests(QueryTestFixture fixture)
+        {
+            Context = fixture.Context;
+            Mapper = fixture.Mapper;
+        }
+
+        public async Task GetNoteDetailsQueryHandlerTests_Success()
+        {
+            // Arrange
+            var handler = new GetNoteDetailsQueryHandler(Context, Mapper);
+
+            // Act
+            var result = await handler.Handle(
+                new GetNoteDetailsQuery
+                {
+                    UserId = NotesContextFactory.UserBId,
+                    Id = Guid.Parse("9A90EE11-8DE9-4E0C-9D23-A5178F7A83A7")
+                },
+                CancellationToken.None);
+
+            // Assert
+            result.ShouldBeOfType<NoteDetailsVm>();
+            result.Title.ShouldBe("Title2");
+            result.CreationDate.ShouldBe(DateTime.Today);
+        }
+    }
+}
