02e24f942ddb3d83244e6f92fcd0413d9fcb15c0
Lex Talionis <lextalionis1488@gmail.com>
2022-05-20 07:45:33 +0100

    Add delete notes tests

---
+++ Notes.Tests/Notes/Commands/DeleteNoteCommandHandlerTests.cs
@@ -0,0 +1,77 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Notes.Application.Common.Exceptions;
+using Notes.Application.Notes.Commands.DeleteCommand;
+using Notes.Tests.Common;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace Notes.Tests.Notes.Commands
+{
+    public class DeleteNoteCommandHandlerTests : TestCommandBase
+    {
+        [Fact]
+        public async Task DeleteNoteCommandHandler_Success()
+        {
+            // Arrange
+            var handler = new DeleteNoteCommandHandler(Context);
+
+            // Act
+            await handler.Handle(
+                new DeleteNoteCommand 
+                    {
+                        Id = NotesContextFactory.NoteIdForDelete,
+                        UserId = NotesContextFactory.UserAId
+                    }, 
+                CancellationToken.None);
+
+            // Assert
+            Assert.Null(Context.Notes.SingleOrDefaultAsync(x =>
+            x.Id == NotesContextFactory.NoteIdForDelete));
+        }
+
+        [Fact]
+        public async Task DeleteNoteCommandHandler_FailOnWrongNoteId()
+        {
+            // Arrange
+            var handler = new DeleteNoteCommandHandler(Context);
+
+            // Act
+
+
+            // Assert
+            await Assert.ThrowsAsync<NotFoundException>(async () =>
+                await handler.Handle(new DeleteNoteCommand
+                {
+                    Id = Guid.NewGuid(),
+                    UserId = NotesContextFactory.UserAId
+                },
+                CancellationToken.None)
+            );
+        }
+
+        [Fact]
+        public async Task DeleteNoteCommandHandler_FailOnWrongUserId()
+        {
+            // Arrange
+            var handler = new DeleteNoteCommandHandler(Context);
+
+            // Act
+
+
+            // Assert
+            await Assert.ThrowsAsync<NotFoundException>(async () =>
+                await handler.Handle(new DeleteNoteCommand
+                {
+                    Id = NotesContextFactory.NoteIdForDelete,
+                    UserId = Guid.NewGuid()
+                },
+                CancellationToken.None)
+            );
+        }
+    }
+}
